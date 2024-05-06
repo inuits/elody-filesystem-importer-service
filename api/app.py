e2024edac6ad0ce7d571776bd7aaba0134ec8f4c
pirooz <pirooz@inuits.eu>
2024-05-06 14:50:11 +0200

    searching folder to find csv files in it

--- api/app.py
+++ api/app.py
@@ -93,12 +93,11 @@ except ModuleNotFoundError:
 load_queues(logger)
 import resources.queues
 
-from resources.importer import Importer, ImporterDirectories, ImporterStart
+from resources.importer import Importer, ImporterDirectories
 from resources.spec import OpenAPISpec
 
-api.add_resource(Importer, "/import")
 api.add_resource(ImporterDirectories, "/importer/directories")
-api.add_resource(ImporterStart, "/importer/start")
+api.add_resource(Importer, "/importer/start")
 api.add_resource(OpenAPISpec, "/spec/inuits-dams-filesystem-importer-service.json")
 
 if __name__ == "__main__":
--- api/resources/importer.py
+++ api/resources/importer.py
@@ -1,14 +1,11 @@
 import os
 
-from app import policy_factory, rabbit
+from app import policy_factory, rabbit, logger
 from flask import request
 from flask_restful import abort, Resource
 from inuits_policy_based_auth import RequestContext
 from services.collection_api_service import CollectionApiService
 from elody.util import signal_upload_file
-from cloudevents.conversion import to_dict
-from cloudevents.http import CloudEvent
-
 
 class Importer(Resource):
     upload_source = os.getenv("UPLOAD_SOURCE", "/mnt/media-import")
@@ -22,10 +19,20 @@ class Importer(Resource):
     def post(self):
         path = self.upload_source
         request_body = self.__get_request_body()
-        request_path = os.path.join(
-            request_body["selected-folder"], request_body["selected-file"]
-        )
+        if 'selected_folder' not in request_body:
+            abort(400, message="Missing 'selected_folder' in request body")
+
+        selected_folder = request_body["selected_folder"]
+        folder_path = os.path.join(self.upload_source, selected_folder.removeprefix("/"))
+        csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+
+        if len(csv_files) != 1:
+            abort(400, message=f"Expected exactly 1 CSV file in {selected_folder}, found {len(csv_files)}")
+
+        selected_file = csv_files[0]
+        request_path = os.path.join(selected_folder, selected_file)
         path = os.path.join(self.upload_source, request_path.removeprefix("/"))
+
         if not os.path.exists(path):
             abort(400, message=f"{path} not found")
 
@@ -34,25 +41,8 @@ class Importer(Resource):
         collection_api_service = CollectionApiService()
         collection_api_service.validate(data)
         upload_links = collection_api_service.get_upload_link(data)
-        signal_upload_file(rabbit, upload_links, request_body["selected-folder"])
-
-class ImporterStart(Resource):
-    def __get_request_body(self):
-        if request_body := request.get_json(silent=True):
-            return request_body
-        abort(405, message="Invalid input")
-
-    @policy_factory.apply_policies(RequestContext(request, ["start-importer"]))
-    def post(self):
-        request_body = self.__get_request_body()
-        attributes = {"type": "dams.import_requested", "source": "dams"}
-        data = {
-            "selected_folder": request_body["selected_folder"],
-            "user": policy_factory.get_user_context().email,
-        }
-        event = to_dict(CloudEvent(attributes, data))
-        rabbit.send(event, routing_key="dams.import_requested")
-        return event, 201
+        logger.info(upload_links)
+        signal_upload_file(rabbit, upload_links, selected_folder)
 
 
 class ImporterDirectories(Resource):
